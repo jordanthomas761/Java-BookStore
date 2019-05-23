5cb114b007b37d05d0debe73af7b48866924b000
Jordan Thomas <jordanthomas761@gmail.com>
2019-05-22 23:32:49 -0400

    added delete books method
    added delete books method

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -52,13 +52,16 @@ public class ControllerServlet extends HttpServlet {
 			switch(action) {
 				case "/admin":
 					 showBookAdmin(request, response);
-           break;
+					 break;
 			  case "/new":
 					showNewForm(request, response);
-          break;
+					break;
 				case "/insert":
 					insertBook(request, response);
-          break;
+					break;
+				case "/delete":
+					insertBook(request, response);
+					break;
         default:
 				   listBooks(request, response);
            break;
@@ -103,6 +106,13 @@ public class ControllerServlet extends HttpServlet {
 
 		bookDAO.insertBook(newBook);
 		response.sendRedirect("list");
+	}
+	
+	private void deleteBook(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException{
+		int id = Integer.parseInt(request.getParameter("id"));
+		bookDAO.deleteBook(id);
+		
 	}
 
 	/**
